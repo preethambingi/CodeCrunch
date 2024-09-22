456fa7f689a0fe0d89b59da26d09de963b9edebb
Preetham Bingi <53289018+preethambingi@users.noreply.github.com>
2024-09-21 22:09:13 -0600

    Update 3. Counting Bits.py

--- Problem Sets/NeetCiode 150/Bit Manipulation/3. Counting Bits.py
+++ Problem Sets/NeetCiode 150/Bit Manipulation/3. Counting Bits.py
@@ -3,6 +3,7 @@ https://leetcode.com/problems/counting-bits/
 ----------------------------------------------------------------------------------------------
 
 Problem Statement:
+
 Given an integer n, we have to return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
 
 ----------------------------------------------------------------------------------------------
