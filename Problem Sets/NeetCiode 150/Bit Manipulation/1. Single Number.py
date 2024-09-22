cb8c5b9f87ec0d5cff0bafc142e3ba956fc524d3
Preetham Bingi <53289018+preethambingi@users.noreply.github.com>
2024-09-21 22:07:28 -0600

    Update 1. Single Number.py

--- Problem Sets/NeetCiode 150/Bit Manipulation/1. Single Number.py
+++ Problem Sets/NeetCiode 150/Bit Manipulation/1. Single Number.py
@@ -8,9 +8,7 @@ We are given a non-empty array of integers nums, where every element appears twi
 ----------------------------------------------------------------------------------------------
 
 Approach 1: Using Dictionary (Hashmap)
-
 The first method that comes up to the mind is storing the frequencies of the elements and then return the element with frequency 1.
-
 We can use dictionaries in python to store the frequencies of the eements.
   
 class Solution:
@@ -36,7 +34,6 @@ The space required is O(n) because we store the frequencies of the all the nums 
 ----------------------------------------------------------------------------------------------
 
 Approach 2: Using Sorting
-
 The second method that comes up to nind is using sorting. After sorting we traverse the list and check if one of the adjacent element is equal to current element. if it's not equal then we can return the element.
 
 class Solution:
@@ -59,10 +56,10 @@ The space complexity is constant because we are not storing anything.
 ----------------------------------------------------------------------------------------------
 
 Approach 3: Using Bitwise XOR Operator (Most Efficient Approach)
-
 The best approach that comes to mind is using XOR operator. 
 To get better understanding about how it works let's look at some XOR properties:
-- X ^ 0 = 0, X ^ X = 0, X ^ X ^ Y = Y
+X ^ 0 = 0, X ^ X = 0, X ^ X ^ Y = Y
+
 Using this properties we will solve the problem.
 
   class Solution:
@@ -74,7 +71,7 @@ Using this properties we will solve the problem.
         
         return res
 
-Complexity Analysis:
+Complexity Analysis
 
 -> Time complexity: O(n)
 The time complexity is O(n) because the traverse the entire list.
